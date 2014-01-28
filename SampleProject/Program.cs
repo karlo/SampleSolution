e83a84f6b2b74178784f976b82c187e85075fef4
Karlo Tadic <Karlo.Tadic@careerbuilder.com>
2014-01-27 22:00:02 -0400

    Test solution commit

---
+++ SampleProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using SampleProject.Calc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Sum(10, 10));
+        }
+    }
+}
---
+++ SampleProjectTest/Calc/CalculatorTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleProject.Calc
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestSumSuccess()
+        {
+            Calculator calc = new Calculator();
+            Assert.AreEqual(20,calc.Sum(10, 10));
+        }
+    }
+}
